a6806d1bf73fda2ded2f119f74de92f7da6def4e
Chris Newman <cnewman@cpointe-inc.com>
2017-03-04 16:03:41 -0500

    Inventory module working where we left off. New polymer cli was breaking things.

---
+++ inventory/inventory-ui/src/main/webapp/inventory/properties_base/farmhacker-properties.js
@@ -0,0 +1,7 @@
+var inventoryServiceBaseURL = "http://localhost:8080";
+
+appProperties = {
+
+	inventoryServiceGetCatalogEntriesServicePath : inventoryServiceBaseURL + "/inventory/rest/catalogService/getCatalogEntries",
+	inventoryServiceCreateCatalogEntryServicePath : inventoryServiceBaseURL + "/inventory/rest/catalogService/create"
+}
